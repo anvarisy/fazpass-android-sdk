12b30a58ded8428e390bbcb92a77b6fca0d256fe
Anvarisy <anvarisy@gmail.com>
2022-05-12 17:01:58 +0700

    Finish verication

--- app/src/main/kotlin/com/fazpass/sample/MainActivity.kt
+++ app/src/main/kotlin/com/fazpass/sample/MainActivity.kt
@@ -1,35 +1,64 @@
 package com.fazpass.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.fazpass.otp.Fazpass
 
 
-private lateinit var myButton: AppCompatButton
-    private lateinit var myPhone: EditText
+private lateinit var btnGenerate: AppCompatButton
+private lateinit var btnVerification: AppCompatButton
+private lateinit var myPhone: EditText
+private lateinit var myOtp: EditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myButton = findViewById(R.id.btnGenerate)
+        btnGenerate = findViewById(R.id.btnGenerate)
         myPhone = findViewById(R.id.edtPhone)
-        //set listener
-        myButton.setOnClickListener {
+        myOtp = findViewById(R.id.edtOtp)
+        btnVerification = findViewById(R.id.btnVerification)
+        val m = Fazpass.initialize("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoxM30.SbTzA7ftEfUtkx0Rdt_eoXrafx1X9kf2SHccS_G5jS8")
+
+        btnGenerate.setOnClickListener {
             //Action perform when the user clicks on the button.
             var phone: String = myPhone.text.toString()
-            val m = Fazpass.initialize("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoxM30.SbTzA7ftEfUtkx0Rdt_eoXrafx1X9kf2SHccS_G5jS8")
-            m.setGateway("9bbd5a07-fc1c-402e-8424-86cb970d0bf")
+            m.setGateway("9bbd5a07-fc1c-402e-8424-86cb970d0bf7")
             m.generateOtp(phone) { response->
-                Fazpass.LoginPage(this, response)
+                /*Fazpass.LoginPage(this, response)*/
+                response.data?.otp?.let { otp -> Log.v("Otp", otp) }
+                response.data?.id?.let { id -> Log.v("Id", id) }
             }
-            
+
+        }
+
+        btnVerification.setOnClickListener {
             m.verifyOtp("",""){status->
                 if(status){
-                    TODO()
+                    Log.v("Status","Verification complete !")
                 }
             }
         }
     }
 }
+
+/*
+        val m = Fazpass.initialize("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoxM30.SbTzA7ftEfUtkx0Rdt_eoXrafx1X9kf2SHccS_G5jS8")
+
+ */
+
+/*      m.setGateway("9bbd5a07-fc1c-402e-8424-86cb970d0bf")
+        m.generateOtp(phone) { response->
+                *//*Fazpass.LoginPage(this, response)*//*
+                response.data?.otp?.let { otp -> Log.v("Otp", otp) }
+            }*/
+
+/*
+        m.verifyOtp("",""){status->
+             if(status){
+                Log.v("Status","Verification complete !")
+              }
+            }
+*/
