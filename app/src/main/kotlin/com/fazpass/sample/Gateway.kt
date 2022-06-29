e61d1ce8236ee6963a2106f24e2499b25297f611
Andri nova riswanto <anvarisy@gmail.com>
2022-06-29 20:18:52 +0700

    Update gateway

--- app/src/main/kotlin/com/fazpass/sample/Gateway.kt
+++ app/src/main/kotlin/com/fazpass/sample/Gateway.kt
@@ -12,11 +12,11 @@ class MyGateway{
     companion object{
        var gateways = arrayListOf(
            Gateway("Choose your gateway","", "fazpass"),
-           Gateway("SMS","98b5d429-b081-4332-ab33-ae1daa746f03", "sms"),
-           Gateway("WHATSAPP","2c7aafdc-7d6c-49c5-95b8-f8476996865c", "whatsapp"),
-           Gateway("EMAIL","11475737-ae16-43de-8253-2a2c0241415f", "email"),
-           Gateway("MISCALL","9bbd5a07-fc1c-402e-8424-86cb970d0bf7","call"),
-           Gateway("LONG WA","e954f844-f644-46ec-917f-0677fc609cee", "whatsapp"),
+           Gateway("SMS","", "sms"),
+           Gateway("WHATSAPP","", "whatsapp"),
+           Gateway("EMAIL","", "email"),
+           Gateway("MISCALL","","call"),
+           Gateway("LONG WA","", "whatsapp"),
        )
     }
 }
